b4b2e09dee3179450315714b6a4334cc475794f1
Norayr Gharibyan <norayr.gharibyan@awtg.co.uk>
2024-05-13 15:59:13 +0400

    first commit

---
+++ src/main/java/am/devvibes/buyandsell/BuyAndSellApplication.java
@@ -0,0 +1,13 @@
+package am.devvibes.buyandsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyAndSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyAndSellApplication.class, args);
+	}
+
+}
