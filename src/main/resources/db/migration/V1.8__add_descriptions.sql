ffc51b9ae3eb7c38bc05ef56d1be37234652efbc
Norayr <norayr.1.gharibyan@iaggbs.com>
2024-05-30 21:09:59 +0400

    Added pgSync

---
+++ src/main/resources/db/migration/V1.8__add_descriptions.sql
@@ -0,0 +1,4 @@
+INSERT INTO description_entity (id, header, category_id) VALUES (1, 'SPECIFICATIONS', 1);
+INSERT INTO description_entity (id, header, category_id) VALUES (2, 'EXTERIOR', 1);
+INSERT INTO description_entity (id, header, category_id) VALUES (3, 'INTERIOR', 1);
+INSERT INTO description_entity (id, header, category_id) VALUES (4, 'ADDITIONAL_INFORMATION', 1);
