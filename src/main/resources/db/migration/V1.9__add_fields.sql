e191a50ff347d43c994fea3498b7efa7e5476a49
Norayr <norayr.1.gharibyan@iaggbs.com>
2024-05-30 21:16:06 +0400

    Added fields.

---
+++ src/main/resources/db/migration/V1.9__add_fields.sql
@@ -0,0 +1,5 @@
+INSERT INTO field_entity (id, field_name, is_prefilled, is_required, measurement_id, description_id) VALUES (1, 'mark', false, true, null, 1);
+INSERT INTO field_entity (id, field_name, is_prefilled, is_required, measurement_id, description_id) VALUES (2, 'model', false, true, null, 1);
+INSERT INTO field_entity (id, field_name, is_prefilled, is_required, measurement_id, description_id) VALUES (3, 'generation', false, true, null, 4);
+INSERT INTO field_entity (id, field_name, is_prefilled, is_required, measurement_id, description_id) VALUES (4, 'exterior color', false, true, null, 3);
+INSERT INTO field_entity (id, field_name, is_prefilled, is_required, measurement_id, description_id) VALUES (5, 'interior color', false, true, null, 2);
